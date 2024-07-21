7cb2d2c2b19559c3462d0a05008d27730284daf6
faustinopsy <rodrigohipnose@gmail.com>
2024-07-20 22:07:42 -0300

    layout componente servicos  e style

--- src/js/paginas/servicos.js
+++ src/js/paginas/servicos.js
@@ -1,6 +1,17 @@
+import  {card} from '../componentes/cards.js'
+import  {blocoText} from '../componentes/blocoText.js'
+const texto1={
+    titulo: "Serviços",
+    paragrafo:"Sites, jogos, aplicativos"
+}
 function servicosComponente(){
     const main = document.querySelector('main')
     main.innerHTML='';
-    main.innerHTML='serviços';
+    main.appendChild(blocoText(texto1));
+    main.appendChild(card());
+    const img1 = document.getElementById('img1')
+    setTimeout(()=>{
+        img1.classList.add('revealed');
+    },100)
 }
 export {servicosComponente};
